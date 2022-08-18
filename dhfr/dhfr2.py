84f3d4660897cbe909c00a918e3e5388e894785e
Zhi Wang <oukore@gmail.com>
2022-08-18 02:17:52 -0500

    bug fix

--- dhfr/dhfr2.py
+++ dhfr/dhfr2.py
@@ -4,7 +4,7 @@ parameters = 'amoeba2013.xml'
 archive = 'dhfr2.arcpdb'
 xyzfile = 'dhfr2.pdb'
 
-nff1, nfft2, nfft3 = 64, 64, 64
+nfft1, nfft2, nfft3 = 64, 64, 64
 ewaldalpha = 0.5446 # 7 Ang
 elecutoff = 0.7*unit.nanometer
 vdwcutoff = 1.2*unit.nanometer
